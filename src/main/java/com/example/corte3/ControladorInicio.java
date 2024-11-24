5bfb4d5a3bf169fdb561a2962e8e07bb756b80e9
Juan Pablo Blanco V. <juanpy5114@gmail.com>
2024-11-23 21:17:30 -0500

    Prueba enviando datos de controlador a la vista por dos metodos

--- src/main/java/com/example/corte3/ControladorInicio.java
+++ src/main/java/com/example/corte3/ControladorInicio.java
@@ -2,22 +2,29 @@
 package com.example.corte3;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Administrador
  */
-@RestController
+@Controller
 @Slf4j
 public class ControladorInicio {
     
+    @Value ("${index.mensaje}")
+    String dato;
     @GetMapping("/")
     
-    public String inicio (){
-        log.info("coroncoro se murio tu maeeeeee'");
-        return "este es un mensaje de ";
+    public String inicio (Model modelo){
+        String mensaje = "Saludos desde Spring MVC";
+        modelo.addAttribute("mensaje", mensaje);
+        modelo.addAttribute("dato", dato);
+        log.info("coroncoro se murio tu maeeeeee");
+        return "index";
         
     }
     
