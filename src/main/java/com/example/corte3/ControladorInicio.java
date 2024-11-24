6112186240e2420bbbc2517045f1fc75b28c7514
Juan Pablo Blanco V. <juanpy5114@gmail.com>
2024-11-23 22:56:28 -0500

    Prueba intercambio de listas y objestos

--- src/main/java/com/example/corte3/ControladorInicio.java
+++ src/main/java/com/example/corte3/ControladorInicio.java
@@ -2,6 +2,8 @@
 package com.example.corte3;
 
 import com.example.corte3.modelo.Usuario;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -24,12 +26,28 @@ public class ControladorInicio {
         String mensaje = "Saludos desde Spring MVC";
         modelo.addAttribute("mensaje", mensaje);
         modelo.addAttribute("dato", dato);
-        Usuario u = new Usuario();
-        u.setCedula("1234 ");
-        u.setClave("Abcd");
-        u.setNombre("Juan Pablo Blanco");
-        u.setEmail("Juanp@gmail.com");
-        modelo.addAttribute("alguien", u);
+        
+        Usuario u1 = new Usuario();
+        u1.setCedula("1234 ");
+        u1.setClave("Abcd");
+        u1.setNombre("Juan Pablo Blanco");
+        u1.setEmail("Juanp@gmail.com");
+        modelo.addAttribute("alguien", u1);
+        
+        Usuario u2 = new Usuario();
+        u2.setCedula("5678 ");
+        u2.setClave("Efgh");
+        u2.setNombre("Cesar manuel blanco");
+        u2.setEmail("CesarM@gmail.com");
+        
+        Usuario u3 = new Usuario();
+        u3.setCedula("0000");
+        u3.setClave("none");
+        u3.setNombre("No bo dy");
+        u3.setEmail("nothing@gmail.com");
+        
+        List <Usuario> listasUsuarios = Arrays.asList(u2,u3);
+        modelo.addAttribute("usuarios",listasUsuarios );
         log.info("coroncoro se murio tu maeeeeee");
         return "index";
         
