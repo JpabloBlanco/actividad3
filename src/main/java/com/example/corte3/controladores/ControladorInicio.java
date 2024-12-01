366f1fde4686124b2190bea8f84cbad3232b69dc
Juan Pablo Blanco V. <juanpy5114@gmail.com>
2024-11-30 19:28:23 -0500

    implementando eliminar

--- src/main/java/com/example/corte3/controladores/ControladorInicio.java
+++ src/main/java/com/example/corte3/controladores/ControladorInicio.java
@@ -55,6 +55,14 @@ public class ControladorInicio {
         return "modificar";
     }
     
+    @GetMapping("/eliminar/{id}")
+    public String eliminar (Usuario usuario, Model modelo){
+        userServicio.eliminar(usuario);
+        return "redirect:/";
+    }
+    
+    
+    
     
     
     
